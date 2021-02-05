e14971f459f08fc027a5b04b4e68eb50fafdd8b1
Titionna-Batista <battit@knights.ucf.edu>
2021-02-05 17:10:07 -0500

    Created Quote App- Fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let quotes = [`"For the sake of comedic integrity, I will refrain from insulting your mother. It is unoriginal to pull from a catalogue of quips which has been visited more  than her very bedroom. -Traveller Orpheus"`, 
-`"Defending a boomer, hm? Interesting. - Anne Howards",` 
-`"When I close my eyes, sometimes I can't see. — Unknown"`];
+let quotes = ["For the sake of comedic integrity, I will refrain from insulting your mother. It is unoriginal to pull from a catalogue of quips which has been visited more  than her very bedroom. -Traveller Orpheus"n, 
+"Defending a boomer, hm? Interesting. - Anne Howards", 
+"When I close my eyes, sometimes I can't see. — Unknown"];
 var quoteResult;
 
 var finalOutcome = Math.floor(Math.random() * quotes.length);
