1e702a9eb6fc20dd09c73dbdd267082ea12acae7
Titionna-Batista <battit@knights.ucf.edu>
2021-02-05 16:56:44 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # personal-pository
-I don't know what else to call it lol.
+
+“Where I’m from, most things blow up eventually. So I learned that when something dope comes along, you gotta lock it down! If you’re always frozen in fear and taking too long to figure out what to do, you’ll miss your opportunity, and maybe get sucked into the propeller of a swamp boat.”
+ – Jason Mendoza, The Good Place 4×09
+
---
+++ index.js
@@ -0,0 +1,9 @@
+var quotes = ["For the sake of comedic integrity, I will refrain from insulting your mother. It is unoriginal to pull from a catalogue of quips which has been visited more  than her very bedroom. -Traveller Orpheus", 
+"Defending a boomer, hm? Interesting. - Anne Howards", 
+"When I close my eyes, sometimes I can't see. — Unknown"];
+var quoteResult;
+
+var finalOutcome = Math.floor(Math.random() * quotes.length);
+console.log(finalOutcome, quotes[finalOutcome]);
+
+
